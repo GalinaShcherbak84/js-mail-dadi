034e1de2060b663806cba18c5df0d182fb80836b
GalinaSherbak84 <galinasherbak@gmail.com>
2021-03-22 17:27:40 +0100

    email

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 //Chiedere al utente la sua mail.
 var mail = prompt('Inserisci il tuo indirizzo email.');
 document.getElementById('email').innerHTML = "Indirizzo email inserito: " + mail;
-//lista di chi può accedere 
-var lista = ['tecno@gmail.com', 'marcocar@libero.it', 'dipiù@gmail.com' ];
+//Lista di chi può accedere. 
+var lista = ['tecno@gmail.com', 'marcocar@libero.it', 'dipiù@gmail.com','lina@mail.ru' ];
 console.log(lista);
+//Controllare che la mail inserita dal utente sia nella lista.
+var user;
+for (var i = 0; i < lista.length; i++){
+    console.log(lista[i]);
+    if (lista[i] === mail) {
+        user = true;
+        break
+    }
+}
+    if (user ===true){
+        document.getElementById('answer').innerHTML = 'Questo indirizzo è presente nella lista. Puoi accedere.'
+    } else {
+        document.getElementById('answer').innerHTML = "Questo indirizzo non è presente nella lista. l'accesso è negato."
+    }
