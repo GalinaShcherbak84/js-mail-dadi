973b036c077118608bc7a6c127cd988b479915ff
GalinaSherbak84 <galinasherbak@gmail.com>
2021-03-22 16:42:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//Chiedere al utente la sua mail.
+var mail = prompt('Inserisci il tuo indirizzo email.');
+document.getElementById('email').innerHTML = "Indirizzo email inserito: " + mail;
+//lista di chi può accedere 
+var lista = ['tecno@gmail.com', 'marcocar@libero.it', 'dipiù@gmail.com' ];
+console.log(lista);
