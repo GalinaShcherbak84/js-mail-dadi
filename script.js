8257e54bdf01ad1ff0147800bcbf33789ca1d66b
GalinaSherbak84 <galinasherbak@gmail.com>
2021-03-22 18:35:08 +0100

    Gioco dei dadi

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Chiedere al utente la sua mail.
+/*//Chiedere al utente la sua mail.
 var mail = prompt('Inserisci il tuo indirizzo email.');
 document.getElementById('email').innerHTML = "Indirizzo email inserito: " + mail;
 //Lista di chi può accedere. 
@@ -18,3 +18,23 @@ for (var i = 0; i < lista.length; i++){
     } else {
         document.getElementById('answer').innerHTML = "Questo indirizzo non è presente nella lista. l'accesso è negato."
     }
+*/
+
+
+    //Gioco dei dadi.
+    //Generare un numero random da 1 a 6 per il giocatore.
+    var giocatore = Math.floor(Math.random() * 6) + 1;
+    document.getElementById('numero-giocatore').innerHTML = "il tuo numero è " + giocatore;
+    //Generare un numero random da 1 a 6 per il computer.
+    var computer = Math.floor(Math.random() * 6) + 1;
+    document.getElementById('numero-computer').innerHTML = "il numero del computer è " + computer;
+    if(giocatore > computer){
+        document.getElementById('vincitore').innerHTML = "Hai vinto!";
+    } else if( giocatore < computer){
+        document.getElementById('vincitore').innerHTML = "Hai perso!";
+    } else{
+        document.getElementById('vincitore').innerHTML = "Pareggio!";
+    }
+
+
+
